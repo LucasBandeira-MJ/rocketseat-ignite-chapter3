62f07e832e2665f5b130697e548a9d52f8c9aabf
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-01 09:44:30 -0300

    Styled the Home page of the application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import styles from '../styles/home.module.sass'
 import Head from 'next/head'
 
 export  default function Home() {
@@ -6,9 +7,17 @@ export  default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Coding girl" />
+      </main>
     </>
   )
 }
