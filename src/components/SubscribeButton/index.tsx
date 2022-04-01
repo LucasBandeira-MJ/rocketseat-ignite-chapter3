126893cbc0280b4cb5fea0064924e3af2ab2a6c0
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-01 10:41:36 -0300

    Added SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.sass'
+
+export const SubscribeButton = () => {
+    return (
+        <button
+            type="button"
+            className={styles.subscribeButton}
+        >
+            Subscribe Now
+        </button>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/home.module.sass'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export  default function Home() {
   return (
@@ -10,11 +11,15 @@ export  default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome!</span>
+
           <h1>News about the <span>React</span> world.</h1>
+
           <p>
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Coding girl" />
       </main>
